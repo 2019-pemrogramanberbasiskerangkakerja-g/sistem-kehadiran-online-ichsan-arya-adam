c726cf03fff77f4f37c1e36c1b82b3b640cc21d6
Unknown <alfianadam97@gmail.com>
2019-04-28 22:17:57 +0700

    register and login without session

--- app.js
+++ app.js
@@ -21,38 +21,112 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // for parsing multipart/form-data
 app.use(upload.array()); 
 
+var sess;
 var mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true)
 mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
 
 var personSchema = mongoose.Schema({
     name: String,
     age: Number,
     nationality: String
- });
+});
 
- var Person = mongoose.model("Person", personSchema);
+var userSchema = mongoose.Schema({
+    name: String,
+    noInduk: {type: Number, unique: true},
+    password: String,
+    role: String
 
+});
 
-
-
-
-
+userSchema.pre('save', function (next) {
+    var self = this;
+    User.find({name : self.username}, function (err, docs) {
+        if (!docs.length){
+            next();
+        }else{                
+            console.log('user exists: ',self.username);
+            next(new Error("User exists!"));
+        }
+    });
+});
+var Person = mongoose.model("Person", personSchema);
+var User = mongoose.model("User", userSchema);
 
 
 app.get('/', function (req, res) {
     res.render('login');
 });
 
+
+app.post('/login', function (req, res) {
+    var user = req.body;
+
+    User.find({noInduk: user.noInduk}, function(err, response){
+        if (err) throw err;
+        if (response.length > 0 ){
+            console.log('berhasil login');
+            sess = req.session;
+            sess.noInduk = req.body.noInduk;
+            res.cookie('SESSION_MHS', req.body.noInduk, {maxAge: 9000000, httpOnly: true }) //2.5 jam
+        }
+    });
+    res.redirect('anggota');
+});
+
 app.get('/register', function (req, res) {
     res.render('register');
 });
 
-app.get('/person', function(req, res){
-    res.render('person');
- });
+app.get('/anggota', function(req, res){
+    sess = req.session;
+    if (sess.page_views){
+        sess.page_views++;
+        res.send('Hi ' + sess.name + ' You visited this page ' + sess.page_views + ' times' );
+    } else if (sess.page_views === 1) {
+        res.send('Hi ' + sess.name + ' You visited this page for the first times' );
+    } else {
+        res.redirect('/');
+    }
+});
 
- app.post('/person', function(req, res){
+app.get('/mahasiswa/register', function(req, res){
+    res.render('register');
+});
+
+app.post('/mahasiswa/add', function(req, res){
+    var user = req.body; //Get the parsed information
+
+    if(!user.name || !user.noInduk || !user.password){
+       res.render('anggota',{
+           error : 'Sorry, you provided worng info'
+       });
+       console.log('sorry salah cuk');
+    } else {
+       var newUser = new User({
+          noInduk: user.noInduk,
+          name: user.name,
+          password: user.password,
+          role: user.role
+       });
+         
+       newUser.save(function(err, User){
+          if(err)
+            return res.send('Error');
+          else{
+            console.log('berhasil');
+            res.redirect('/')
+
+          } 
+       });
+    }
+
+});
+
+app.post('/person', function(req, res){
     var personInfo = req.body; //Get the parsed information
+
     console.log(personInfo);
     if(!personInfo.name || !personInfo.age || !personInfo.nationality){
        res.render('anggota', personInfo.name, personInfo.age, personInfo.nationality);
@@ -68,14 +142,12 @@ app.get('/person', function(req, res){
             console.log(err);
           else{
             console.log('berhasil');
-            res.render('anggota', personInfo);
+            res.render('anggota', Person);
 
           } 
        });
     }
- });
-
-
+});
 
 app.listen(3000, function (req, res) {
     console.log("App start at port 3000");
